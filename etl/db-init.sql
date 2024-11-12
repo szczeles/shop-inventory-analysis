a7ffc396e312b72ac1322de3539fbd54ddc51ccc
Mariusz Strzelecki <szczeles@gmail.com>
2024-11-12 07:56:12 +0100

    Loading CSVs into DB

---
+++ etl/db-init.sql
@@ -0,0 +1,37 @@
+CREATE TABLE products (
+  product_id SERIAL PRIMARY KEY,
+  upc VARCHAR(14),
+  name TEXT,
+  item_number INT,
+  price NUMERIC(18,2),
+  supplier TEXT,
+  inventory_level INT,
+  inventory_updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW(),
+
+  UNIQUE (upc)
+);
+
+CREATE TYPE alternate_type AS ENUM ('variant', 'case');
+
+CREATE TABLE product_alternates (
+  product_alternate_id SERIAL PRIMARY KEY,
+  product_id INT,
+  upc VARCHAR(14),
+  alternate_type alternate_type,
+  case_pack REAL,
+  created_at TIMESTAMP DEFAULT NOW(),
+
+  CONSTRAINT fk_product
+      FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+);
+
+COPY products(product_id,upc,name,item_number,price,supplier,inventory_level,inventory_updated_at)
+FROM '/data/products.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY product_alternates(product_alternate_id,product_id,upc,alternate_type,case_pack)
+FROM '/data/product_alternates.csv'
+DELIMITER ',' CSV HEADER;
